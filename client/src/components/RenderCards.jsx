0a9db94d35eb8a63184b330419eb37592b649f3e
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-18 12:44:15 -0600

    feat(RenderCards): implement complete RenderCards component

--- client/src/components/RenderCards.jsx
+++ client/src/components/RenderCards.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Card } from "../components";
 
-const RenderCards = () => {
-  return <div>RenderCards</div>;
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-l uppercase">{title}</h2>
+  );
 };
 
 export default RenderCards;
