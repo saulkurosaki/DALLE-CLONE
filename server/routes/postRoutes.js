1f2dad6bc56e206acc87a0b4f4895299d4f154c4
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-19 10:39:47 -0600

    feat(postRoutes.js): implement postRoutes file initial layout

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../mongodb/models/post";
+
+dotenv.config();
+
+const router = express.Router();
