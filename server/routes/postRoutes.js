28b93451bb273d8571c886e257c5cf176bf08d4f
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-20 00:21:56 -0600

    feat(postRoutes.js): little get all posts and create a post functions refactor

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -18,28 +18,34 @@ cloudinary.config({
 router.route("/").get(async (req, res) => {
   try {
     const posts = await Post.find({});
-
     res.status(200).json({ success: true, data: posts });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
   }
 });
 
 // CREATE A POST
 router.route("/").post(async (req, res) => {
   try {
-    const { prompt, name, photo } = req.body;
+    const { name, prompt, photo } = req.body;
     const photoUrl = await cloudinary.uploader.upload(photo);
 
     const newPost = await Post.create({
       name,
       prompt,
-      photo: photoUrl.photo,
+      photo: photoUrl.url,
     });
 
-    res.status(201).json({ success: true, data: newPost });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
   }
 });
 
