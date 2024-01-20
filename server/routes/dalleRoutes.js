48ff8d6902c51b6bc8dc5a92f922e3ee158e33ce
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-19 22:52:45 -0600

    fix(dalleRoutes): fix some little openai api errors

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -26,12 +26,10 @@ router.route("/").post(async (req, res) => {
       size: "1024x1024",
       response_format: "b64_json",
     });
-
     const image = aiResponse.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
     res.status(500).send(error?.response.data.error.message);
   }
 });
